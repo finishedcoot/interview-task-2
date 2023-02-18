15b414be15300bbb490fdc1ca50438388df4ed95
Ardeshir <ardeshiir@outlook.com>
2023-02-18 15:13:03 +0330

    [ADD] - add locale context

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,11 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import LocaleContext from "@/Components/LocaleContext";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return(
+      <LocaleContext>
+        <Component {...pageProps} />
+      </LocaleContext>
+        )
 }
